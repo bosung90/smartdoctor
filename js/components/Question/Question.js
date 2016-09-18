bc493b5f3e754c8ddfa99c16e53a952d6a624212
Eric Kim <bosung90@gmail.com>
2016-09-18 13:04:43 -0700

    Added 4 more questions with submit button

--- js/components/Question/Question.js
+++ js/components/Question/Question.js
@@ -7,26 +7,37 @@ import {
   StyleSheet,
   TextInput,
   Image,
+  ScrollView,
 } from 'react-native'
 import YesNoQuestion from './YesNoQuestion'
 
 export default class Question extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <YesNoQuestion selection={2} question='1. Do you feel nausia/vomiting lately?' />
-        <YesNoQuestion selection={2} question='2. Do you experience frequent heartburn?' />
-        <YesNoQuestion selection={2} question='3. Do you feel a lost of appetite?' />
+      <ScrollView style={styles.scrollView} containerStyle={styles.scrollViewContent}>
+        <YesNoQuestion selection={1} question='1. Do you feel nausia/vomiting lately?' />
+        <YesNoQuestion selection={1} question='2. Do you experience frequent heartburn?' />
+        <YesNoQuestion selection={1} question='3. Do you feel a lost of appetite?' />
         <YesNoQuestion selection={2} question='4. Do you experience constant bloating?' />
-      </View>
+        <YesNoQuestion selection={1} question='5. Do you experience early satiety?' />
+        <YesNoQuestion selection={1} question='6. Do you see blood in the stool?' />
+        <YesNoQuestion selection={1} question='7. Do you experience excessive fatigue?' />
+        <YesNoQuestion selection={2} question='8. Do you experience stomach pain?' />
+
+        <TouchableOpacity style={[styles.button, {alignSelf: 'center', backgroundColor: '#E6AF45', alignItems: 'center', elevation: 6, marginTop: 40,}]} onPress={this.props.handleGetStarted}>
+          <Text style={{color: 'white', fontSize: 17}}>Submit</Text>
+        </TouchableOpacity>
+      </ScrollView>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
+  scrollView: {
     backgroundColor: '#448FAA',
+  },
+  scrollViewContent: {
+    ...StyleSheet.absoluteFillObject,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -38,5 +49,5 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     paddingHorizontal: 20,
     marginVertical: 20,
-  }
+  },
 })
